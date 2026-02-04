e9fa70a1ba92d2d91adfb1e54bc812ac24d484ef
Cameron Campbell <cameron.campbell325@gmail.com>
2026-02-04 23:48:58 +0000

    corner rounded smoothing for selections

--- src/views/chat/blocks/chat_area/existing_chat.rs
+++ src/views/chat/blocks/chat_area/existing_chat.rs
@@ -106,7 +106,8 @@ impl RenderOnce for ChatMessage {
         let selectable_content =
             SelectableText::new(self.id.with_suffix("content"), selectable_content_state)
                 .selection_color(selection_color)
-                .selection_rounded(px(4.))
+                .selection_rounded(px(6.))
+                .selection_rounded_smoothing(1.)
                 .w_auto()
                 .max_w_full()
                 .word_wrap(true)
